3d2755ae9b1dbc871075f6be38802061ac4374b6
cgray <cal.j.gray@gmail.com>
2019-05-13 00:27:59 +0800

    first commit

---
+++ test/TravelStopTest.java
@@ -0,0 +1,80 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TravelStopTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Test method for {@link TravelStop#TravelStop(double, double, java.lang.String, java.lang.String)}.
+   */
+  @Test
+  public void testTravelStop() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link TravelStop#getLatitude()}.
+   */
+  @Test
+  public void testGetLatitude() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link TravelStop#getLongitude()}.
+   */
+  @Test
+  public void testGetLongitude() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link TravelStop#getStreet()}.
+   */
+  @Test
+  public void testGetStreet() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link TravelStop#getSuburb()}.
+   */
+  @Test
+  public void testGetSuburb() {
+    fail("Not yet implemented");
+  }
+
+}
